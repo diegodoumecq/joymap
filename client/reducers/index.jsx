814f8cc81304cdaea67fd51bd806402ca35d9481
diegodoumecq <licenciado.diego@gmail.com>
2016-08-01 02:20:56 -0300

    removed client folder

--- client/reducers/index.jsx
+++
@@ -1,9 +0,0 @@
-
-import { combineReducers } from 'redux'
-import { routeReducer as routing } from 'redux-simple-router'
-import todos from './todos'
-
-export default combineReducers({
-  routing,
-  todos
-})
