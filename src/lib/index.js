24995dc57cb2b4065c4d72aaed6c56c59850524a
diegodoumecq <licenciado.diego@gmail.com>
2016-10-31 03:31:09 -0300

    Fixed index.js import/export

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,2 @@
-import P from './Player';
-import JoyMap from './JoyMap';
-
-// TODO check why I can't just do export Player;
-export const Player = P;
-export default JoyMap;
+export default from './JoyMap';
+export Player from './Player';
