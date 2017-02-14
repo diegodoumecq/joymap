24e0850a06df4457aff2e1bcc7a5487769518fce
diegodoumecq <licenciado.diego@gmail.com>
2017-02-14 02:00:28 -0300

    Added support for multiple gamepads to StateLog example

--- examples/StateLog/StateLog.js
+++ examples/StateLog/StateLog.js
@@ -1,4 +1,4 @@
-import { join, compact } from 'lodash/fp';
+import { join, compact, forEach } from 'lodash/fp';
 import {
     stringifyInputs, countPressed, renderRows, stringifyMappers
 } from './utils';
@@ -16,11 +16,46 @@ document.getElementById('app').innerHTML = `
         </header>
         <div class="log-example">
             <p>Suggestion: Open the dev console</p>
-            <div id="logs">
-                <span class="row">Psssst, hey, connect a gamepad and use it. You'll see.</span>
-            </div>
         </div>
     </article>`;
+
+function createPlayer(joyMap, padId) {
+    const p = joyMap.addPlayer(padId);
+
+    // Set custom buttons
+    p.setButton('Jump', p.getButtonIndexes('A', 'X', 'Y', 'L2', 'R2'));
+    p.setButton('Shoot', p.getButtonIndexes('B'));
+    p.setButton('LookUp', p.getButtonIndexes('dpadUp'));
+    p.setButton('LookDown', p.getButtonIndexes('dpadDown'));
+    p.setButton('LookLeft', p.getButtonIndexes('dpadLeft'));
+    p.setButton('LookRight', p.getButtonIndexes('dpadRight'));
+    p.setButton('StickAverage', p.getStickIndexes('L', 'R'));
+
+    // Set mappers
+    p.setMapper('Point', ({ player }) => player.getSticks('R').pressed);
+    p.setMapper('MovePoint', ({ player }) => countPressed(player.getSticks()));
+    p.setMapper('CountFace', ({ player }) => countPressed(player.getButtons('A', 'B', 'X', 'Y')));
+    p.setMapper('CountAll', ({ pad, player }) => {
+        const buttonCount = countPressed(pad.buttons);
+        const stickCount = countPressed(player.getSticks());
+
+        if (buttonCount || stickCount) {
+            return `Btn:${buttonCount} Sticks:${stickCount}`;
+        }
+
+        return null;
+    });
+
+    const element = document.createElement('section');
+    element.className = 'player';
+    const id = p.getPadId();
+    element.innerHTML = `
+        <div class="name">Gamepad: ${id}</div>
+        <div id="${id}">Waiting for inputs...</div>
+    `;
+
+    document.querySelector('.log-example').appendChild(element);
+}
 
 // Flags used to show/hide output separated by input type
 const showButtons = true;
@@ -31,66 +66,47 @@ const showMappers = true;
 const joyMap = createJoyMap({
     threshold: 0.2,
     onPoll() {
-        const jo = joyMap.getPlayers()[0];
-        // On each frame render to HTML and console.log the state of buttons, sticks and mappers
-        const compilation = [
-            !showButtons ? '' : stringifyInputs(jo.getButtons()),
-            !showSticks ? '' : stringifyInputs(jo.getSticks()),
-            !showMappers ? '' : stringifyMappers(jo.getMappers())
-        ];
+        const unusedIds = joyMap.getUnusedPadIds();
 
-        const stringOutput = join(', ', compact(compilation));
-
-        if (stringOutput) {
-            // Log string output to dev console
-            console.log(stringOutput); // eslint-disable-line
-
-            // Re-render this last output into HTML
-            document.getElementById('logs').innerHTML = renderRows(compact([
-                !showButtons ? null : {
-                    inputType: 'buttons',
-                    compilation: compilation[0],
-                    displayName: 'Buttons'
-                },
-                !showSticks ? null : {
-                    inputType: 'sticks',
-                    compilation: compilation[1],
-                    displayName: 'Sticks'
-                },
-                !showMappers ? null : {
-                    inputType: 'mappers',
-                    compilation: compilation[2],
-                    displayName: 'Mappers'
-                }
-            ]));
+        if (unusedIds.length > 0) {
+            forEach(padId => createPlayer(joyMap, padId), unusedIds);
         }
+
+        forEach(player => {
+            // On each frame render to HTML and console.log the state of buttons, sticks and mappers
+            const compilation = [
+                !showButtons ? '' : stringifyInputs(player.getButtons()),
+                !showSticks ? '' : stringifyInputs(player.getSticks()),
+                !showMappers ? '' : stringifyMappers(player.getMappers())
+            ];
+
+            const stringOutput = join(', ', compact(compilation));
+
+            if (stringOutput) {
+                // Log string output to dev console
+                console.log(stringOutput); // eslint-disable-line
+
+                // Re-render this last output into HTML
+                document.getElementById(player.getPadId()).innerHTML = renderRows(compact([
+                    !showButtons ? null : {
+                        inputType: 'buttons',
+                        compilation: compilation[0],
+                        displayName: 'Buttons'
+                    },
+                    !showSticks ? null : {
+                        inputType: 'sticks',
+                        compilation: compilation[1],
+                        displayName: 'Sticks'
+                    },
+                    !showMappers ? null : {
+                        inputType: 'mappers',
+                        compilation: compilation[2],
+                        displayName: 'Mappers'
+                    }
+                ]));
+            }
+        }, joyMap.getPlayers());
     }
-});
-
-const jo = joyMap.addPlayer();
-
-// Set custom buttons
-jo.setButton('Jump', jo.getButtonIndexes('A', 'X', 'Y', 'L2', 'R2'));
-jo.setButton('Shoot', jo.getButtonIndexes('B'));
-jo.setButton('LookUp', jo.getButtonIndexes('dpadUp'));
-jo.setButton('LookDown', jo.getButtonIndexes('dpadDown'));
-jo.setButton('LookLeft', jo.getButtonIndexes('dpadLeft'));
-jo.setButton('LookRight', jo.getButtonIndexes('dpadRight'));
-jo.setButton('StickAverage', jo.getStickIndexes('L', 'R'));
-
-// Set mappers
-jo.setMapper('Point', ({ player }) => player.getSticks('R').pressed);
-jo.setMapper('MovePoint', ({ player }) => countPressed(player.getSticks()));
-jo.setMapper('CountFace', ({ player }) => countPressed(player.getButtons('A', 'B', 'X', 'Y')));
-jo.setMapper('CountAll', ({ pad, player }) => {
-    const buttonCount = countPressed(pad.buttons);
-    const stickCount = countPressed(player.getSticks());
-
-    if (buttonCount || stickCount) {
-        return `Btn:${buttonCount} Sticks:${stickCount}`;
-    }
-
-    return null;
 });
 
 console.log( // eslint-disable-line
@@ -98,7 +114,7 @@ console.log( // eslint-disable-line
     'color: green; font-weight: bold; font-size: 1.5em;'
 );
 console.log( // eslint-disable-line
-    '%c We got everything you want honey, as long as it\'s printing all of your gamepad input',
+    '%c We got everything you want honey, as long as it\'s printing all of your gamepad\'s input',
     'color: green; font-weight: bold; font-size: 1.5em;'
 );
 
