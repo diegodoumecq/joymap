fef24d2d84f8eb06740f94db95694db7e974372e
diegodoumecq <licenciado.diego@gmail.com>
2026-03-06 23:55:28 -0300

    tmp 3

--- examples/Main/components/IframeCard.tsx
+++ examples/Main/components/IframeCard.tsx
@@ -6,7 +6,7 @@ interface IframeCardProps {
 
 export function IframeCard({ children }: IframeCardProps) {
   return (
-    <div className="overflow-hidden rounded-lg border border-border bg-secondary/50">
+    <div className="flex min-h-0 flex-1 flex-col overflow-hidden rounded-lg border border-border bg-secondary/50">
       <div className="min-h-0 w-full flex-1">{children}</div>
     </div>
   );
